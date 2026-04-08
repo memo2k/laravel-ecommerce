4c026272008e62bc7a105e5f0779c6f01b82b3cd
Mehmed <mehmedm325@gmail.com>
2026-04-08 21:27:22 +0300

    add ProductController

--- app/Http/Controllers/Site/ProductController.php
+++ app/Http/Controllers/Site/ProductController.php
@@ -3,26 +3,68 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductCategory;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        $products = $products ?? collect(range(1, 12))->map(function ($i) {
-            return (object) [
-                'id' => $i,
-                'name' => "Demo product $i",
-                'category' => ['Accessories', 'Shoes', 'Bags', 'Jackets'][($i - 1) % 4],
-                'price' => 19 + ($i * 3),
-                'old_price' => $i % 3 === 0 ? (29 + ($i * 3)) : null,
-            ];
-        });
-
-        $categories = $categories ?? ['Accessories', 'Shoes', 'Bags', 'Jackets', 'T-Shirts'];
+        $search = trim((string) request('q', ''));
         $selectedCategory = request('category');
         $sortBy = request('sort', 'newest');
-        $search = request('q', '');
+        $minPrice = request('min_price');
+        $maxPrice = request('max_price');
+
+        $categories = ProductCategory::query()
+            ->orderBy('name')
+            ->get(['id', 'name']);
+
+        $productsQuery = Product::query()
+            ->with('productCategory')
+            ->where('is_active', true);
+
+        if ($search !== '') {
+            $productsQuery->where(function ($query) use ($search) {
+                $query->where('name', 'like', "%{$search}%")
+                    ->orWhere('description', 'like', "%{$search}%")
+                    ->orWhereHas('productCategory', function ($categoryQuery) use ($search) {
+                        $categoryQuery->where('name', 'like', "%{$search}%");
+                    });
+            });
+        }
+
+        if (!empty($selectedCategory)) {
+            $productsQuery->where('product_category_id', $selectedCategory);
+        }
+
+        if ($minPrice !== null && $minPrice !== '') {
+            $productsQuery->where('price', '>=', (float) $minPrice);
+        }
+
+        if ($maxPrice !== null && $maxPrice !== '') {
+            $productsQuery->where('price', '<=', (float) $maxPrice);
+        }
+
+        switch ($sortBy) {
+            case 'price_asc':
+                $productsQuery->orderBy('price');
+                break;
+            case 'price_desc':
+                $productsQuery->orderByDesc('price');
+                break;
+            case 'name_asc':
+                $productsQuery->orderBy('name');
+                break;
+            case 'newest':
+            default:
+                $sortBy = 'newest';
+                $productsQuery->latest();
+                break;
+        }
+
+        $products = $productsQuery->get();
 
         return view('pages.site.products.products_list', [
             'products' => $products,
@@ -33,8 +75,15 @@ class ProductController extends Controller
         ]);
     }
 
-    public function show()
+    public function show(Request $request)
     {
-        return view('pages.site.products.product');
+        $product = Product::query()
+            ->with('productCategory')
+            ->where('slug', $request->slug)
+            ->firstOrFail();
+
+        return view('pages.site.products.product', [
+            'product' => $product,
+        ]);
     }
 }
