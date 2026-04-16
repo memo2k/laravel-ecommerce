4f4da28ebc7dd865114572d66944dd070ae127a6
Mehmed <mehmedm325@gmail.com>
2026-04-16 20:42:33 +0300

    implement checkout logic

--- app/Http/Controllers/Site/CheckoutController.php
+++ app/Http/Controllers/Site/CheckoutController.php
@@ -2,15 +2,98 @@
 
 namespace App\Http\Controllers\Site;
 
+use App\Constants\OrderStatusConstant;
+use App\Constants\PaymentMethodConstant;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use App\Models\OrderProduct;
 use App\Repositories\CartRepository;
+use App\Services\CartService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class CheckoutController extends Controller
 {
+    protected $cartRepository;
+    protected $cartService;
+
+    public function __construct(CartRepository $cartRepository, CartService $cartService)
+    {
+        $this->cartRepository = $cartRepository;
+        $this->cartService = $cartService;
+    }
+
     public function index()
     {
-        $cartData = (new CartRepository())->getCartData();
-        return view('pages.site.checkout', ['cartData' => $cartData]);
+        $cartData = $this->cartRepository->getCartData();
+
+        if ($cartData['totalProducts'] === 0) {
+            return redirect()->route('cart.index');
+        }
+
+        return view('pages.site.checkout.checkout', ['cartData' => $cartData]);
+    }
+
+    public function store(Request $request)
+    {
+        $rules = [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|max:255',
+            'phone' => 'required|string|max:40',
+            'address' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'zip' => 'required|string|max:255',
+            'country' => 'required|string|max:255',
+            'payment_method' => 'required|in:'.implode(',', array_keys(PaymentMethodConstant::PAYMENT_METHODS)),
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $cartData = $this->cartRepository->getCartData();
+
+        $order = Order::create([
+            'user_id' => Auth::user()->id,
+            'total_amount' => $cartData['totalPrice'],
+            'status' => OrderStatusConstant::PENDING,
+            'payment_method' => PaymentMethodConstant::PAYMENT_METHODS[$request->payment_method],
+            'delivery_address' => $request->address,
+            'city' => $request->city,
+            'zip' => $request->zip,
+            'country' => $request->country,
+            'customer_email' => $request->email,
+            'customer_phone' => $request->phone,
+            'customer_first_name' => $request->first_name,
+            'customer_last_name' => $request->last_name,
+            'customer_notes' => $request->notes,
+        ]);
+
+        foreach ($cartData['items'] as $item) {
+            OrderProduct::create([
+                'order_id' => $order->id,
+                'product_id' => $item['product_id'],
+                'quantity' => $item['quantity'],
+                'price' => $item['price'],
+                'total' => $item['price'] * $item['quantity'],
+            ]);
+        }
+
+        $this->cartService->clearCart();
+
+        return redirect()->route('checkout.order-summary', ['id' => $order->id])->with('success', 'Order created successfully');
+    }
+
+    public function orderSummary(Request $request)
+    {
+        $order = Order::query()
+            ->with(['orderProducts.product'])
+            ->findOrFail($request->query('id'));
+
+        return view('pages.site.checkout.order_summary', ['order' => $order]);
     }
 }
