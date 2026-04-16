4a22e289a2b2c93460fdfdf523c82f31d82d025a
Mehmed <mehmedm325@gmail.com>
2026-04-16 21:33:49 +0300

    add caching for products front view

--- app/Http/Controllers/Site/ProductController.php
+++ app/Http/Controllers/Site/ProductController.php
@@ -3,13 +3,10 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
-use App\Models\Cart;
-use App\Models\CartProduct;
 use App\Models\Product;
 use App\Models\ProductCategory;
-use App\Repositories\CartRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 
 class ProductController extends Controller
 {
@@ -21,73 +18,93 @@ class ProductController extends Controller
         $minPrice = request('min_price');
         $maxPrice = request('max_price');
 
-        $categories = ProductCategory::query()
-            ->orderBy('name')
-            ->get(['id', 'name']);
+        $cacheKey = 'products.index.'.md5(json_encode([
+            'q' => $search,
+            'category' => $selectedCategory,
+            'sort' => $sortBy,
+            'min_price' => $minPrice,
+            'max_price' => $maxPrice,
+        ]));
 
-        $productsQuery = Product::query()
-            ->with('productCategory')
-            ->where('is_active', true);
+        if (Cache::has($cacheKey)) {
+            $viewParams = Cache::get($cacheKey);
+        } else {
+            $categories = ProductCategory::query()
+                ->orderBy('name')
+                ->get(['id', 'name']);
 
-        if ($search !== '') {
-            $productsQuery->where(function ($query) use ($search) {
-                $query->where('name', 'like', "%{$search}%")
-                    ->orWhere('description', 'like', "%{$search}%")
-                    ->orWhereHas('productCategory', function ($categoryQuery) use ($search) {
-                        $categoryQuery->where('name', 'like', "%{$search}%");
-                    });
-            });
+            $productsQuery = Product::query()
+                ->with('productCategory')
+                ->where('is_active', true);
+
+            if ($search !== '') {
+                $productsQuery->where(function ($query) use ($search) {
+                    $query->where('name', 'like', "%{$search}%")
+                        ->orWhere('description', 'like', "%{$search}%")
+                        ->orWhereHas('productCategory', function ($categoryQuery) use ($search) {
+                            $categoryQuery->where('name', 'like', "%{$search}%");
+                        });
+                });
+            }
+
+            if (!empty($selectedCategory)) {
+                $productsQuery->where('product_category_id', $selectedCategory);
+            }
+
+            if ($minPrice !== null && $minPrice !== '') {
+                $productsQuery->where('price', '>=', (float) $minPrice);
+            }
+
+            if ($maxPrice !== null && $maxPrice !== '') {
+                $productsQuery->where('price', '<=', (float) $maxPrice);
+            }
+
+            $resolvedSort = $sortBy;
+            switch ($sortBy) {
+                case 'price_asc':
+                    $productsQuery->orderBy('price');
+                    break;
+                case 'price_desc':
+                    $productsQuery->orderByDesc('price');
+                    break;
+                case 'name_asc':
+                    $productsQuery->orderBy('name');
+                    break;
+                case 'newest':
+                default:
+                    $resolvedSort = 'newest';
+                    $productsQuery->latest();
+                    break;
+            }
+
+            $viewParams = [
+                'products' => $productsQuery->get(),
+                'categories' => $categories,
+                'selectedCategory' => $selectedCategory,
+                'sortBy' => $resolvedSort,
+                'search' => $search,
+            ];
+
+            Cache::put($cacheKey, $viewParams, now()->addMinutes(15));
         }
 
-        if (!empty($selectedCategory)) {
-            $productsQuery->where('product_category_id', $selectedCategory);
-        }
-
-        if ($minPrice !== null && $minPrice !== '') {
-            $productsQuery->where('price', '>=', (float) $minPrice);
-        }
-
-        if ($maxPrice !== null && $maxPrice !== '') {
-            $productsQuery->where('price', '<=', (float) $maxPrice);
-        }
-
-        switch ($sortBy) {
-            case 'price_asc':
-                $productsQuery->orderBy('price');
-                break;
-            case 'price_desc':
-                $productsQuery->orderByDesc('price');
-                break;
-            case 'name_asc':
-                $productsQuery->orderBy('name');
-                break;
-            case 'newest':
-            default:
-                $sortBy = 'newest';
-                $productsQuery->latest();
-                break;
-        }
-
-        $products = $productsQuery->get();
-
-        return view('pages.site.products.products_list', [
-            'products' => $products,
-            'categories' => $categories,
-            'selectedCategory' => $selectedCategory,
-            'sortBy' => $sortBy,
-            'search' => $search,
-        ]);
+        return view('pages.site.products.products_list', $viewParams);
     }
 
     public function show(Request $request)
     {
-        $product = Product::query()
-            ->with('productCategory')
-            ->where('slug', $request->slug)
-            ->firstOrFail();
+        $cacheKey = 'products.show.'.md5($request->slug);
+        if (Cache::has($cacheKey)) {
+            $product = Cache::get($cacheKey);
+        } else {
+            $product = Product::query()
+                ->with('productCategory')
+                ->where('slug', $request->slug)
+                ->firstOrFail();
 
-        return view('pages.site.products.product', [
-            'product' => $product,
-        ]);
+            Cache::put($cacheKey, $product, now()->addMinutes(15));
+        }
+
+        return view('pages.site.products.product', ['product' => $product]);
     }
 }
