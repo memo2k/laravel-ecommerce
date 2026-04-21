a7b0e8a51616475a52434bd6fcade3592ab0fa47
Mehmed <mehmedm325@gmail.com>
2026-04-21 20:31:56 +0300

    Add SKU field to products table for unique identification

--- database/migrations/2026_03_30_184410_create_products_table.php
+++ database/migrations/2026_03_30_184410_create_products_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('product_category_id');
+            $table->string('sku')->unique();
             $table->string('name');
             $table->string('slug')->unique();
             $table->string('description');
