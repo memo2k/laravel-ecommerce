0bc1117081c7cbe9f1b4cce47b3d9da0a0f602ae
Mehmed <mehmedm325@gmail.com>
2026-04-28 21:19:54 +0300

    update admin routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AttributeController;
 use App\Http\Controllers\Admin\ProductCategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProductController;
@@ -7,6 +8,7 @@ use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\PermissionController;
+use App\Http\Controllers\Admin\SelectController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
@@ -16,6 +18,8 @@ Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(
     Route::get('/product/edit/{id?}', [ProductController::class, 'edit'])->name('product.edit');
     Route::post('/product/save', [ProductController::class, 'save'])->name('product.save');
     Route::delete('/product/delete', [ProductController::class, 'delete'])->name('product.delete');
+    Route::post('/product/add-attribute-option', [ProductController::class, 'addAttributeOption'])->name('product.add-attribute-option');
+    Route::post('/product/remove-attribute-option', [ProductController::class, 'removeAttributeOption'])->name('product.remove-attribute-option');
 
     Route::get('/orders', [OrderController::class, 'index'])->name('orders');
     Route::get('/order/view/{id}', [OrderController::class, 'view'])->name('order.view');
@@ -38,4 +42,13 @@ Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(
     Route::get('/product-category/edit/{id?}', [ProductCategoryController::class, 'edit'])->name('product-category.edit');
     Route::post('/product-category/save', [ProductCategoryController::class, 'save'])->name('product-category.save');
     Route::delete('/product-category/delete', [ProductCategoryController::class, 'delete'])->name('product-category.delete');
+    Route::post('/product-category/add-attribute', [ProductCategoryController::class, 'addAttribute'])->name('product-category.add-attribute');
+    Route::post('/product-category/remove-attribute', [ProductCategoryController::class, 'removeAttribute'])->name('product-category.remove-attribute');
+
+    Route::get('/attributes', [AttributeController::class, 'index'])->name('attributes');
+    Route::get('/attribute/edit/{id?}', [AttributeController::class, 'edit'])->name('attribute.edit');
+    Route::post('/attribute/save', [AttributeController::class, 'save'])->name('attribute.save');
+    Route::delete('/attribute/delete', [AttributeController::class, 'delete'])->name('attribute.delete');
+
+    Route::get('/select', [SelectController::class, 'index'])->name('select');
 });
