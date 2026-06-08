c4484559565d7f4e651176c89b0335d89d118266
Mehmed <mehmedm325@gmail.com>
2026-06-08 22:02:53 +0300

    Update admin routes to include access control for admin users

--- routes/admin.php
+++ routes/admin.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Admin\SelectController;
 use App\Http\Controllers\Admin\SettingController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+Route::middleware(['auth', 'verified', 'can:access-admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
         
     Route::get('/products', [ProductController::class, 'index'])->name('products');
