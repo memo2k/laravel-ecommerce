3f31048630a96cc7f4320dcbf2d0f4ac80a17a8f
Mehmed <mehmedm325@gmail.com>
2026-06-08 21:58:38 +0300

    Refactor routes

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,8 @@
 use App\Http\Controllers\Site\ProfileController;
 use App\Http\Controllers\Site\CartController;
 use App\Http\Controllers\Site\CheckoutController;
-use App\Http\Controllers\Site\HomepageController;
 use App\Http\Controllers\Site\ProductController as SiteProductController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', [HomepageController::class, 'index'])->name('homepage');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
@@ -19,10 +16,10 @@ Route::middleware('auth')->group(function () {
 Route::prefix('checkout')->name('checkout.')->group(function () {
     Route::get('/', [CheckoutController::class, 'index'])->name('index');
     Route::post('/', [CheckoutController::class, 'store'])->name('store');
-    Route::get('/order-summary', [CheckoutController::class, 'orderSummary'])->name('order-summary');
+    Route::get('/order-summary/{order}', [CheckoutController::class, 'orderSummary'])->name('order-summary');
 });
 
-Route::get('/products', [SiteProductController::class, 'index'])->name('products.index');
+Route::get('/', [SiteProductController::class, 'index'])->name('products.index');
 Route::get('/product/{slug}', [SiteProductController::class, 'show'])->name('product.show');
 
 Route::prefix('cart')->name('cart.')->group(function () {
